b49dd6e6f5d4b7432c60044fedde93c591a129b1
harshinisandireddy-droid <harshini_sandireddy@srmap.edu.in>
2026-03-29 11:10:32 +0530

    Add files via upload

---
+++ linear.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target)
+            return i; 
+    }
+    return -1; 
+}
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 7};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 7;
+
+    int result = linearSearch(arr, size, target);
+
+    if (result != -1)
+        printf("Element found at index %d\n", result);
+    else
+        printf("Element not found\n");
+
+    return 0;
+}
